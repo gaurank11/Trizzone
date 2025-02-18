736292d731aa37845b3bacc2fbb8597bdab50230
gaurank11 <sharmagaurank63@gmail.com>
2025-02-18 14:01:13 +0530

    first commit

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -3,15 +3,10 @@ import { useNavigate } from 'react-router-dom';
 
 function Hero() {
   const images = ["/h1.jpg", "/h2.jpg", "/h3.jpg"];
-  const quotes = [
-    "Design is intelligence made visible.",
-    "Architecture starts when you put two bricks together.",
-    "Simplicity is the ultimate sophistication.",
-  ];
+  
 
   const navigate = useNavigate();
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  const [currentQuoteIndex, setCurrentQuoteIndex] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -32,7 +27,7 @@ function Hero() {
       />
 
       {/* Left Side Buttons */}
-      <div className="absolute right-8 md:left-12 bottom-28 md:bottom-1/4 flex flex-row  md:space-x-4">
+      <div className="absolute right-8 md:left-12 bottom-28 md:bottom-20 flex flex-row  md:space-x-4">
         {["Architectures", "Interiors", "Landscape"].map((text, index) => (
           <button
             key={index}
@@ -45,13 +40,10 @@ function Hero() {
         ))}
       </div>
 
-      {/* Center Quotes */}
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-xl font-medium text-center bg-black/50 px-6 py-2 rounded-lg">
-        {quotes[currentQuoteIndex]}
-      </div>
+    
 
       {/* Right Side Big "TRIZZONE" */}
-      <div className="absolute right-12 bottom-1/4 text-white text-5xl md:text-7xl font-bold uppercase">
+      <div className="absolute right-12 bottom-1/4 md:bottom-20 text-white text-5xl md:text-7xl font-bold uppercase">
         TRIZZONE
       </div>
     </div>
