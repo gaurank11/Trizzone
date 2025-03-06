c65506323971725501239ace259bfda5e8a19b52
gaurank11 <sharmagaurank63@gmail.com>
2025-03-06 23:15:24 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import Loader from "./components/loading";
 import Home from "./pages/Home";
 import PropertiesPage from "./components/properties";
 import Contact from "./pages/Contact";
+import Hero1 from "./components/hero1";
 
 function App() {
   return (
    
       <Routes>
         {/* Show Loader initially and navigate to Home */}
-        <Route path="/" element={<Loader />} />
+        {/* <Route path="/" element={<Loader />} />
         <Route path="/home" element={<Home />} />
         <Route path="/properties" element={<PropertiesPage />} />
-        <Route path="/contact" element={<Contact/>} />
+        <Route path="/contact" element={<Contact/>} /> */}
+
+        <Route path="/" element={<Hero1 />} />
       </Routes>
 
   );
